92c2b86cfa3d98f74545135b3115dced70920839
laijy <1004670104@qq.com>
2020-07-04 17:11:30 +0800

    初次提交

--- movie-web/src/main/java/com/cn/share/movie/web/WebApplication.java
+++ movie-web/src/main/java/com/cn/share/movie/web/WebApplication.java
@@ -2,12 +2,13 @@ package com.cn.share.movie.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 public class WebApplication {
 
